e27cba8e1eeee19b69e24cec5bc1538f6b320cb9
berkesasa <berkesasa11@gmail.com>
2023-12-22 22:29:03 +0300

    updated CounterPage

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,57 +1,74 @@
-import { useReducer } from 'react'
-import Button from '../components/Button'
-import Panel from '../components/Panel'
+import { useReducer } from 'react';
+import Button from '../components/Button';
+import Panel from '../components/Panel';
+
+const INCREMENT_COUNT = "increment"
+const SET_VALUE_TO_ADD = "set-value-to-add"
+const DECREMENT_COUNT = "decrement"
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 const reducer = (state, action) => {
-    if (action.type === "increment") {
-        return {
-            ...state,
-            count: state.count + 1,
-        }
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1,
+            };
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1,
+            };
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0,
+            };
+        case SET_VALUE_TO_ADD:
+            return {
+                ...state,
+                valueToAdd: action.payload,
+            };
+        default:
+            return state;
     }
-    if (action.type === "change-value-to-add") {
-        return {
-            ...state,
-            valueToAdd: action.payload
-        }
-    }
-
-    return state
-}
+};
 
 function CounterPage({ initialCount }) {
-
     // const [count, setCount] = useState(initialCount);
     // const [valueToAdd, setValueToAdd] = useState(0);
     const [state, dispatch] = useReducer(reducer, {
         count: initialCount,
-        valueToAdd: 0
-    })
+        valueToAdd: 0,
+    });
+    console.log(state);
 
     const increment = () => {
         dispatch({
-            type: "increment"
-        })
-    }
-
+            type: INCREMENT_COUNT,
+        });
+    };
     const decrement = () => {
-        // setCount(count - 1)
-    }
-
+        dispatch({
+            type: DECREMENT_COUNT,
+        });
+    };
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
 
         dispatch({
-            type: 'change-value-to-add',
+            type: SET_VALUE_TO_ADD,
             payload: value,
         });
     };
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        // setCount(count + valueToAdd);
-        // setValueToAdd(0)
-    }
+
+        dispatch({
+            type: ADD_VALUE_TO_COUNT,
+        });
+    };
 
     return (
         <Panel className="m-3">
@@ -75,4 +92,4 @@ function CounterPage({ initialCount }) {
     );
 }
 
-export default CounterPage
+export default CounterPage;
